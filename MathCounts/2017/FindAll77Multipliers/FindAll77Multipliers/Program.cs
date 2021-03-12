90b68ee0721d2ae7a1968866777881e3fd7acd3c
Tao Cao <tao_cao@hotmail.com>
2021-03-11 20:11:24 -0800

    MathCount 2017

---
+++ MathCounts/2017/FindAll77Multipliers/FindAll77Multipliers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FindAll77Multipliers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> result = new List<int>();
+            int count = 1;
+            for (int i4 = 0; i4 <= 1; i4++)
+            {
+                for (int i3 = 0; i3 <= 1; i3++)
+                {
+                    for (int i2 = 0; i2 <= 1; i2++)
+                    {
+                        for (int i1 = 0; i1 <= 1; i1++)
+                        {
+                            for (int i0 = 0; i0 <= 1; i0++)
+                            {
+                                int n = 700000 + i4 * 70000 + i3 * 7000 + i2 * 700 + i1 * 70 + i0 * 7;
+                                if (n%77 == 0)
+                                {
+                                    Console.WriteLine($"{count}: {n} = {n/77} * 77");
+                                    count++;
+                                }
+                            }
+                        }
+
+                    }
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MathCounts/2017/Tiles/Tiles/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tiles
+{
+    public class Tile
+    {
+        public char letter { get; set; }
+        public int number { get; set; }
+
+        public Tile (char c, int i)
+        {
+            letter = c;
+            number = i;
+        }
+
+    }
+
+    class Program
+    {
+        private static Random rng = new Random();
+
+        static void Main(string[] args)
+        {
+            Trial();
+        }
+
+        static bool Trial()
+        {
+            char[] letters = { 'A', 'B', 'C', 'D', 'E' };
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            List<Tile> pile = new List<Tile>();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    pile.Add(new Tile(letters[i], numbers[j]));
+                }
+            }
+
+            Shuffle(pile);
+
+            var tile0 = pile[0];
+            var tile1 = pile[1];
+            var tile2 = pile[2];
+
+            Console.WriteLine($"1: {tile0.letter}{tile0.number}, 2: {tile1.letter}{tile1.number}, 3: {tile2.letter}{tile2.number}");
+
+            if ((InCommon(tile0, tile1) || InCommon(tile0, tile2)) && 
+
+
+            return true;
+        }
+
+        static bool InCommon(Tile t1, Tile t2)
+        {
+            if (t1.letter == t2.letter || t1.number == t2.number)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void Shuffle<T>(IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
